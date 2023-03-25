9e814dd4d55cf2d0df171ca9ee54d619d80c9472
Artsiom Kondratovich <artsiom.kondratovich@billwerk.com>
2023-03-25 15:58:13 +0300

    finish statistics components

--- DTO/TaskDTO.cs
+++ DTO/TaskDTO.cs
@@ -7,10 +7,10 @@ public class TaskDTO
     [ObjectId] public string ProjectId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public TaskStatus Status { get; set; }
 }
 
 public class TaskReadDTO : TaskDTO
 {
     public string Id { get; set; }
-    public TaskStatus Status { get; set; }
 }
