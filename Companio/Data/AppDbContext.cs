956ba046608bf26d5b1a3871792a4cdc578de6ec
Artsem Kandratovich <artsem.kandratovich@modsen-software.com>
2025-05-18 13:29:04 +0300

    add efcore and openapi config, remove startup class

---
+++ Companio/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Companio.Models;
+using Microsoft.EntityFrameworkCore;
+using Task = Companio.Models.Task;
+
+namespace Companio.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<AbsenceTimeline> AbsenceTimelines { get; set; }
+    public DbSet<AuthenticationResult> AuthenticationResults { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<User> Users { get; set; }
+}
